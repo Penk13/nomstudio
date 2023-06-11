c0644d098b7a34f2a4f26cd6a2c71282af9d3c30
Penk13 <natanaelsteven109@gmail.com>
2023-06-11 23:31:00 +0700

    improvement

--- core/models.py
+++ core/models.py
@@ -12,9 +12,7 @@ class WorkCategory(models.Model):
 class Work(models.Model):
     category = models.ForeignKey(WorkCategory, on_delete=models.CASCADE)
     title = models.CharField(max_length=255, blank=False)
-    description = models.TextField(blank=True)
     image = models.ImageField(upload_to ='work/')
-    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.category.title + " : " + self.title
